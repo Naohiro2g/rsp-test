6f17a4b5ec18b84dcc1bc605c89a3d48559bcf9c
Naohiro Tsuji <naohiro2g@gmail.com>
2016-02-03 00:06:42 +0900

    change it to from_scratch and to_scratch

---
+++ rsp-from_scratch.py
@@ -0,0 +1,28 @@
+# Receives and prints data sent by Scratch; use control-C to quit
+from array import array
+import socket
+import time
+
+
+HOST = 'localhost'
+PORT = 42001
+
+
+print("connecting...")
+scratchSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+scratchSock.connect((HOST, PORT))
+print("connected! waiting for data...")
+
+
+# print incoming data forever
+
+while True:
+	time.sleep(0.01)
+	data = scratchSock.recv(1024)
+	if not data: break
+	print(data)
+
+print("closing socket...")
+scratchSock.close()
+print("done")
+sys.exit()
