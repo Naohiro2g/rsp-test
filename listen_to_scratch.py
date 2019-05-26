3a34a749e1cf37c740cc0813e8ca63cf4ef7ae7e
naohiro2g <naohiro2g@code2create.club>
2019-05-27 03:14:15 +0900

    talker goes to Python3 version and Japanese compatible

--- listen_to_scratch.py
+++ listen_to_scratch.py
@@ -3,7 +3,7 @@ from array import array
 import socket
 import time
 import codecs
-
+import sys
 
 HOST = 'localhost'
 PORT = 42001
@@ -11,7 +11,7 @@ PORT = 42001
 print("connecting...")
 scratchSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 scratchSock.connect((HOST, PORT))
-print("connected! waiting for data...")
+print("Connected to Scratch! waiting for data...")
 
 # print incoming data forever, ctrl-c to break and quit
 # First four bytes is size of message in byte string format.
@@ -19,16 +19,16 @@ print("connected! waiting for data...")
 # 00 00 00 18 sensor-update "G1" 1234
 data = scratchSock.recv(1024)  # ignore the first garbage
 while True:
-	data = scratchSock.recv(1024)
-	if not data: break
-        count = int(codecs.encode(data[0:4], 'hex'), 16)
-        print ('byte count: ' + str(count) + '  '),
-        data = data[4:]
-	print(data)
-	time.sleep(0.05)
+    data = scratchSock.recv(1024)
+    if not data: break
+    count = int(codecs.encode(data[0:4], 'hex'), 16)
+    print ('bytes received: ' + str(count) + '  '),
+    data = data[4:] + '<EOL>'
+    print(data)
+    time.sleep(0.05)
 
 
 print("closing socket...")
 scratchSock.close()
-print("done")
+print("bye...")
 sys.exit()
--- talk_to_scratch.py
+++ talk_to_scratch.py
@@ -1,39 +1,61 @@
-# Sends data to Scratch;
-# default host is localhost, put an empty message to quit
+# Python 3 version
+# Sends messages to Scratch
+
 from array import array
 import socket
 import time
 import sys
+import struct
+import unicodedata
 
-from Tkinter import Tk
-from tkSimpleDialog import askstring
-root = Tk()
-root.withdraw()
+PORT = 42001         # Scratch Remote Sensors Protocol port
+HOST = 'localhost'   # his-raspi.local or 192.168.1.22
+                     # to connect to another computer on the same network
 
-PORT = 42001
-HOST = askstring('Scratch Connector', 'IP:')
-if not HOST: HOST = 'localhost'
-
-print("connecting...")
+print("\n", "Connecting...")
 scratchSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 scratchSock.connect((HOST, PORT))
-print("connected")
+print("Connected to Scratch!")
 
 def sendScratchCommand(cmd):
-    n = len(cmd)
-    a = array('c')
-    a.append(chr((n >> 24) & 0xFF))
-    a.append(chr((n >> 16) & 0xFF))
-    a.append(chr((n >>  8) & 0xFF))
-    a.append(chr(n & 0xFF))
-    scratchSock.send(a.tostring() + cmd)
+    scratchSock.send(struct.pack(">I",lenCount(cmd)))
+    print(struct.pack(">I",lenCount(cmd)))
+    scratchSock.send(bytes(cmd, 'UTF-8'))
+    print(bytes(cmd, 'UTF-8'))
+
+def lenCount(text):
+    count = 0
+    for c in text:
+        if unicodedata.east_asian_width(c) in 'FWA':
+            count += 3
+        else:
+            count += 1
+    return count
+
+def sensorUpdater():
+    print("Sensor name?  Try 'G1'")
+    variableName = input()
+    print("Value?   Try '1234'  You can check it by [(G1) sensor value] block.")
+    variableValue = input()
+    return (variableName + '" ' + variableValue)
+
+
+print('\n', 'Scratch Remote Sensors Protocol talker started...\n')
+print('    Please try "hello" or "こんにちは" as the test message.')
+print('    Put an empty message to go into sensor-update mode.')
 
 while True:
-    msg = askstring('Scratch Connector', 'Send Broadcast:')
-    if not msg: break
-    sendScratchCommand('broadcast "' + msg + '"')
+    print('\n', 'Input a message to broadcast in Scratch-RSP: ')
+    msg = input()
+    if not msg:
+        msg = sensorUpdater()
+        sendScratchCommand('sensor-update "' + msg + ' ')
+        #print('sensor-update "' + msg + ' ')
+    else:
+        sendScratchCommand('broadcast "' + msg + '"')
+        #print('broadcast "' + msg + '"')
 
 print("closing socket...")
 scratchSock.close()
-print("done")
+print("\n", "bye...")
 sys.exit()
